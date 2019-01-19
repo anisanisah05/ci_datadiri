c53d6518dc6dceb37f00bbff1d4f596332de236f
anisah nabilah <anisah nabilah>
2019-01-19 07:05:00 +0700

    first commit

---
+++ application/views/data_diri.php
@@ -0,0 +1,20 @@
+<html>
+<head> <br> <br>
+<title>PROFILKU</title>
+</head>
+<body style="background-color: 	#D3D3D3;">
+<center><div style="background-color: pink; width: 1050px; height: 440px; margin-top: 10px; margin-left: 250px;">
+<h1 style="color: #2F4F4F;"><br>PROFILKU</h1> <br>
+  <table>
+  <tr>
+      <th><img width="250px" height="300px" src="<?=base_url()?>assets/nb.png">
+  </th>
+<th style="font-size:20px;color: #696969; padding-left: 100px;">
+    Nama : Anisah Nabilah <br> Kelas : XI RPL 2 <br> Absen : 07
+</th>
+</tr>
+</table>
+<br>
+</center></div>
+</body>
+</html>
